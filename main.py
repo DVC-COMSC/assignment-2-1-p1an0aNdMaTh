f013744e7086783efb032350bba52041463dfbb2
p1an0aNdMaTh <ltrinh543@insite.4cd.edu>
2023-06-20 21:56:52 -0700

    Assignment [2-1]

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 def main():
+    m_amount = input('Enter the number of male students registered in a class.')
+    f_amount = input('Enter the number of female students registered in the above class.')
+    total_students = int(m_amount) + int(f_amount)
+    m_perc = ( int(m_amount) / int(total_students) ) 
+    f_perc = ( int(f_amount) / int(total_students) )
+    print ('The total number of students:', total_students)
+    print ('The amount of females:', m_amount)
+    print ('The amount of females:', f_amount)
+    print ('The percentage of males:', format (m_perc, '.2%'))
+    print ('The percentage of females:', format (f_perc, '.2%'))
     """
     ##################################################
-    Comlete your code here
+    Complete your code here
     Use m_perc and f_perc for your results
     ##################################################
     """
